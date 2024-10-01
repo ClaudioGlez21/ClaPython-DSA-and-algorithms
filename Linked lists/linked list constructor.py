13a00ad41160e9fe469a4914db32411225b4448f
Claudio Gonzalez <claudio.glez24@gmail.com>
2024-10-01 17:31:23 -0600

    Constructor created

--- Linked lists/linked list constructor.py
+++ Linked lists/linked list constructor.py
@@ -1,1 +1,22 @@
-print("Hola")
+class Node:
+    def __init__(self,value) -> None: #since all the methods of the linked list need a new node, we create a new class that creates the new node
+        self.value = value
+        self.next = None
+        
+class LinkedList:
+    def __init__(self,value): #Create a node and initialize the linked list
+        new_node = Node(value)
+        self.head= new_node
+        self.tail = new_node
+        self.length=1
+
+    def append(self,value):#create a new node and add the node to the end
+        pass
+    def prepend(self,value): #create a new node and add Node to the beginning
+        pass
+    def insert(self,value): #Create a node and insert it at an specific location
+        pass
+
+mylinkedlist = LinkedList(7)
+print(mylinkedlist.length)
+print(mylinkedlist.head.value)
